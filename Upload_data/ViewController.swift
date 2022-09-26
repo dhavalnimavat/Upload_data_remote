ebce3b5092e5ebf1849202cc75509e401eb80c4b
Dhaval Nimavat <dr.dhaval.nimavat@gmail.com>
2022-09-26 10:39:28 +0530

    Initial Commit

---
+++ Upload_data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Upload_data
+//
+//  Created by Dhaval Nimavat on 26/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
